b65e459643905a680f9ff90e98c3c2c2b3ca78b0
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-12-18 16:44:33 +0700

    [Thực hành] Ứng dụng quản lý danh sách khách hàng

---
+++ src/main/java/com/codegym/service/HibernateCustomerService.java
@@ -0,0 +1,78 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+@Service
+public class HibernateCustomerService implements ICustomerService{
+    private static SessionFactory sessionFactory;
+    private static EntityManager entityManager;
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.conf.xml")
+                    .buildSessionFactory();
+            entityManager = sessionFactory.createEntityManager();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public List<Customer> findAll() {
+        String queryStr = "SELECT c FROM Customer AS c";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Customer customer) {
+
+    }
+
+    @Override
+    public Customer findById(int id) {
+        String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Customer origin = findById(customer.getId());
+            origin.setName(customer.getName());
+            origin.setEmail(customer.getEmail());
+            origin.setAddress(customer.getAddress());
+            session.saveOrUpdate(origin);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+    }
+
+}
